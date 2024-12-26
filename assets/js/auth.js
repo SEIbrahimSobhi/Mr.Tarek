fbe719c74835ea75d8be4b3d2ba304269a925ece
EBRAHEM SOBHY <seebrahemsobhy@gmail.com>
2024-12-26 18:45:30 -0500

    Delete assets/js/auth.js

--- assets/js/auth.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggle-password').addEventListener('click', function () {
-    const passwordInput = document.getElementById('password');
-    const type = passwordInput.type === 'password' ? 'text' : 'password';
-    passwordInput.type = type;
-
-    // تغيير الأيقونة
-    this.name = type === 'password' ? 'eye-outline' : 'eye-off-outline';
-});
