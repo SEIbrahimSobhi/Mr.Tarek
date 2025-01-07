9270ede8b18fcd8e694eab6d0d3194a49b26aead
EBRAHEM SOBHY <seebrahemsobhy@gmail.com>
2025-01-08 01:22:09 +0200

    Add files via upload

---
+++  firebase-admin.js
@@ -0,0 +1,16 @@
+// Import the Firebase Admin SDK
+const admin = require('firebase-admin');
+
+// Load the service account key JSON file
+const serviceAccount = require('./serviceAccountKey.json'); // تأكد من أن هذا المسار صحيح
+
+// Initialize the Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'mr-tarek-2e40f.firebaseapp.com' 
+});
+
+// Reference to your Firestore database
+const db = admin.firestore();
+
+module.exports = db;
