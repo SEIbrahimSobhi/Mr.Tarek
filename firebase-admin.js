c53b9c044900de84d9bd728d52e898f00fd9562c
EBRAHEM SOBHY <seebrahemsobhy@gmail.com>
2025-01-13 17:00:40 -0500

    firebase-admin.js

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,16 +1,20 @@
-// Import the Firebase Admin SDK
-var admin = require('firebase-admin');
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
 
-// Load the service account key JSON file
-var serviceAccount = require('serviceAccountKey.json'); // تأكد من أن هذا المسار صحيح
+// إعدادات التكوين الخاصة بمشروعك
+const firebaseConfig = {
+      apiKey: "AIzaSyDO4K33SYDaeBgYweqsEXni9QQ863DI_Pc",
+      authDomain: "mr-tarek-2e40f.firebaseapp.com",
+      projectId: "mr-tarek-2e40f",
+      storageBucket: "mr-tarek-2e40f.appspot.com",
+      messagingSenderId: "807997869740",
+      appId: "1:807997869740:web:8eca7473d24693bf861909"
+    };
 
-// Initialize the Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'mr-tarek-2e40f.firebaseapp.com' 
-});
+// تهيئة تطبيق Firebase
+const app = initializeApp(firebaseConfig);
 
-// Reference to your Firestore database
-var db = admin.firestore();
+// تهيئة Firestore
+const db = getFirestore(app);
 
-module.exports = db;
+export { db };
