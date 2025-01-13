97b93f5500a5c5bd3c8c5b4986b17bc82a81c369
EBRAHEM SOBHY <seebrahemsobhy@gmail.com>
2025-01-13 11:36:39 -0500

    firebase-admin.js

--- firebase-admin.js
+++ firebase-admin.js
@@ -2,7 +2,7 @@
 const admin = require('firebase-admin');
 
 // Load the service account key JSON file
-const serviceAccount = require('./serviceAccountKey.json'); // تأكد من أن هذا المسار صحيح
+const serviceAccount = require('serviceAccountKey.json'); // تأكد من أن هذا المسار صحيح
 
 // Initialize the Admin SDK
 admin.initializeApp({
